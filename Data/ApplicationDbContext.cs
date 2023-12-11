50e6bd96604105afb2ba282590e6abfd0815e593
chkuchku <150239146+chkuchku@users.noreply.github.com>
2023-12-11 16:23:27 +0400

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FinalProject.Models;
+
+namespace FinalProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
